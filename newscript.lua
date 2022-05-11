aef6fd5d7d3268f9eb23af24f474833f848d8a19
RtxyDev <65295268+RtxyDev@users.noreply.github.com>
2022-05-11 16:51:31 -0400

    Create newscript.lua

---
+++ newscript.lua
@@ -0,0 +1,6 @@
+
+game.Players.LocalPlayer:Destroy()
+print("New script, discord copied to clipboard")
+game.Players.LocalPlayer:Kick("New script, discord copied to clipboard")
+setclipboard("https://discord.gg/maeDuGSA7S")
+
