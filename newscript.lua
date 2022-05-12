74f23e93476eb177474df3ae99583c604bd38901
RtxyDev <65295268+RtxyDev@users.noreply.github.com>
2022-05-12 09:31:50 -0400

    Update newscript.lua

--- newscript.lua
+++ newscript.lua
@@ -1,5 +1,8 @@
 
-game:Shutdown()
+local notify = loadstring(game:HttpGet(('https://pastebin.com/raw/ipvW4CJ5'),true))()
+
+notify.prompt('Ro-Flux', "Please get the new script from the discord, discord copied to clipboard or roflux.net/discord")
+
 print("New script, discord copied to clipboard")
 game.Players.LocalPlayer:Kick("New script, discord copied to clipboard")
 setclipboard("https://discord.gg/maeDuGSA7S")
